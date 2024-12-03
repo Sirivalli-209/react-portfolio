89936a08214971229c782f171f110e153afa3de0
Sirivalli-209 <166811426+Sirivalli-209@users.noreply.github.com>
2024-12-03 18:14:32 +0530

    Add files via upload

---
+++ ProjectData.js
@@ -0,0 +1,79 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://thevirtualinstructor.com/blog/wp-content/uploads/2014/04/build-art-portfolio-website.jpg",
+    title: "E-Commerce Art Website",
+    description:
+      "A fully functional e-commerce art website designed to showcase a diverse collection of original artwork, enabling artists to sell directly to customers through a user-friendly online platform featuring high-resolution image galleries, detailed artwork descriptions, secure payment processing, and intuitive navigation, allowing art enthusiasts to easily browse, select, and purchase pieces from a curated collection while providing a seamless buying experience.",
+    tech_stack: ["Bootstrap","javascript"],
+    github_url: "",
+    
+  },
+  {
+    img: "https://static.wixstatic.com/media/a471fa_80b5453ae0c44030a6d6a509f760dab0~mv2.png/v1/fill/w_560,h_340,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2880.png",
+    title: "Learning platform",
+    description:
+      "A learning platform website in a portfolio could be described as: A user-friendly, interactive online platform designed to facilitate accessible and engaging learning experiences through a curated library of courses, assessments, and personalized learning paths, catering to diverse learners and skill development needs.",
+    tech_stack: ["html", "css","Javascript"],
+    github_url: "",
+    
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1zHQkMg8BficYXntMDEfUAxjUi5xgnIWtIg&s",
+    title: "Portfolio",
+    description:
+      "A portfolio is a compilation of academic and professional materials that exemplifies your beliefs, skills, qualifications, education, training, and experiences. It provides insight into your personality and work ethic.",
+    tech_stack: ["ReactJS","css"],
+    github_url: "",
+    
+  }
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
