d191969ed11dd2ad1f71fca369e9019a3f0b2b0f
Sirivalli-209 <166811426+Sirivalli-209@users.noreply.github.com>
2024-12-03 18:12:35 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Dropdown from "../Dropdown/Dropdown";
+import Header from "../Header/Header";
+import {
+  HeroContainer,
+  HeroWrapper,
+  HeroLeft,
+  HeroRight,
+ 
+  ScrollDown,
+  ScrollLink,
+} from "./HeroElements";
+import { TypeAnimation } from 'react-type-animation';
+import ScrollAnimation from "react-animate-on-scroll";
+
+function Hero() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [showSubtitle, setShowSubtitle] = useState(false);
+  const [showScrollDown, setShowScrollDown] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <main>
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Header toggle={toggle} />
+      <HeroContainer>
+        <HeroWrapper>
+          <HeroLeft>
+            <ScrollAnimation animateIn="fadeIn" >
+              <TypeAnimation
+                cursor={false}
+                sequence={[
+                  'Hi, I\'m Vadde Sirivalli Akshitha.',
+                  () => setShowSubtitle(true)
+                ]}
+                speed={{ type: "keyStrokeDelayInMs", value: 150 }}
+                wrapper="h1"
+                repeat={0}
+              />
+              {showSubtitle &&
+                <TypeAnimation
+                  cursor={true}
+                  sequence={[
+                    'A MERN STACK DEVELOPER.',
+                  ]}
+                  
+                  wrapper="h3"
+                  
+                />
+              }
+            </ScrollAnimation>
+
+          </HeroLeft>
+          <HeroRight>
+            <ScrollAnimation animateIn="fadeIn">
+             
+            </ScrollAnimation>
+          </HeroRight>
+        </HeroWrapper>
+        {showScrollDown &&<ScrollAnimation animateIn="flipInX" offset={0}>
+        <ScrollDown to="projects" id="scrollDown">
+          <ScrollLink>
+            Scroll down
+            <img
+              src="/scroll-down.svg"
+              alt="scroll-down"
+            />
+          </ScrollLink>
+        </ScrollDown>
+        </ScrollAnimation>}
+      </HeroContainer>
+    </main>
+  );
+}
+
+export default Hero;
